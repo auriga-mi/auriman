11eab49a29b4d891ab5d2d2c45f69ecc6498c7c9
auriga <pissot.a@sfr.fr>
2022-02-07 08:07:22 +0100

    First draft of Auriga Manager

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,102 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </div>
+  <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
+    <div class="px-2 sm:px-6 lg:px-8">
+      <div class="relative flex items-center justify-between h-16">
+        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="sr-only">Open main menu</span>
+            <MenuIcon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
+            <XIcon v-else class="block h-6 w-6" aria-hidden="true" />
+          </DisclosureButton>
+        </div>
+        <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+          <div class="flex-shrink-0 flex items-center">
+            <h1 class="font-bold text-white">Auriman</h1>
+          </div>
+          <div class="hidden sm:block sm:ml-6">
+            <div class="flex space-x-4">
+              <router-link v-for="item in navigation" :key="item.name" :to="item.href" :class="[ item.href == currentRouteName ? ' bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'px-3 py-2 rounded-md text-sm font-medium']" :aria-current=" item.href == currentRouteName ? 'page' : undefined">{{ item.name }}</router-link>
+            </div>
+          </div>
+        </div>
+        <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+
+          <!-- Profile dropdown -->
+          <Menu as="div" class="ml-3 relative">
+            <div>
+              <MenuButton class="bg-gray-800 flex text-sm rounded-full text-gray-400 hover:text-white">
+                <span class="sr-only">User Menu</span>
+                <UserIcon class="h-6 w-6" :class="userNavActive ? 'text-white' : ''" aria-hidden="true" />
+              </MenuButton>
+            </div>
+            <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
+              <MenuItems class="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <MenuItem v-for="item in userNavigation" :key="item.name" v-slot="{ active }">
+                  <a :href="item.href" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">{{ item.name }}</a>
+                </MenuItem>
+              </MenuItems>
+            </transition>
+          </Menu>
+        </div>
+      </div>
+    </div>
+
+    <DisclosurePanel class="sm:hidden">
+      <div class="px-2 pt-2 pb-3 space-y-1">
+        <DisclosureButton v-for="item in navigation" :key="item.name" as="a" :href="item.href" :class="[ item.href == currentRouteName ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white', 'block px-3 py-2 rounded-md text-base font-medium']" :aria-current=" item.href == currentRouteName ? 'page' : undefined">{{ item.name }}</DisclosureButton>
+      </div>
+    </DisclosurePanel>
+  </Disclosure>
   <router-view/>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/vue'
+import { UserIcon, MenuIcon, XIcon } from '@heroicons/vue/outline'
 
-#nav {
-  padding: 30px;
+const navigation = [
+  { name: 'Dashboard', href: '/home'},
+  { name: 'About', href: '/about'},
+]
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+const userNavigation = [
+  { name: 'Preferences', href: '/preferences'},
+  { name: 'Settings', href: '/settings'},
+]
 
-    &.router-link-exact-active {
-      color: #42b983;
+export default {
+  components: {
+    Disclosure,
+    DisclosureButton,
+    DisclosurePanel,
+    Menu,
+    MenuButton,
+    MenuItem,
+    MenuItems,
+    UserIcon,
+    MenuIcon,
+    XIcon,
+  },
+  setup() {
+    return {
+      navigation,
+      userNavigation,
+    }
+  },
+  computed: {
+    currentRouteName() {
+      return this.$route.path
+    },
+    userNavActive() {
+      let exists = false
+      for (let item in userNavigation){
+        if (userNavigation[item].href.includes(this.currentRouteName)) {
+          exists = true
+        }
+      }
+      return exists
     }
   }
 }
-</style>
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,56 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
-  </div>
+<div>
+    <div class="grid md:grid-cols-4 sm:grid-cols-2 lg:grid-cols-6 m-5 mb-10">
+        <AppCard v-for="item in applications" :key="item.name" :imgurl="item.imgurl" :description="item.description" :href="item.href" :path="item.path" />
+    </div>
+</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+<script>
+import { defineComponent } from 'vue'
+import AppCard from '@/components/AppCard.vue'
+
+const applications = [
+    { 
+        name: 'Photoshop', 
+        description: 'Graphic Design', 
+        imgurl: 'https://upload.wikimedia.org/wikipedia/commons/a/af/Adobe_Photoshop_CC_icon.svg',
+        path: '"C:\\Program Files\\Adobe\\Adobe Photoshop 2020\\Photoshop.exe"'
+    },
+    { 
+        name: 'InDesign', 
+        description: 'Graphic Design', 
+        imgurl: 'https://upload.wikimedia.org/wikipedia/commons/4/48/Adobe_InDesign_CC_icon.svg',
+        path: '"C:\\Program Files\\Adobe\\Adobe InDesign 2020\\InDesign.exe"'
+    },
+    { 
+        name: 'Illustrator', 
+        description: 'Graphic Design', 
+        imgurl: 'https://upload.wikimedia.org/wikipedia/commons/f/fb/Adobe_Illustrator_CC_icon.svg',
+        path: '"C:\\Program Files\\Adobe\\Adobe Illustrator 2020\\Support Files\\Contents\\Windows\\Illustrator.exe"'},
+    { 
+        name: 'Sony Vegas', 
+        description: 'Video Editing', 
+        imgurl: 'https://upload.wikimedia.org/wikipedia/commons/3/39/Vegas_Pro_15.0.png',
+        path: '"C:\\Program Files\\VEGAS\\VEGAS Pro 15.0\\vegas150.exe"'
+    },
+    { 
+        name: 'VS Code', 
+        description: 'Programming', 
+        imgurl: 'https://upload.wikimedia.org/wikipedia/commons/2/2d/Visual_Studio_Code_1.18_icon.svg',
+        path: '"C:\\Users\\pisso\\AppData\\Local\\Programs\\Microsoft VS Code\\Code.exe"'
+    },
+]
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
-});
+    name: 'Home',
+    components: {
+        AppCard,
+    },
+    setup() {
+        return {
+            applications
+        }
+    }
+})
 </script>
